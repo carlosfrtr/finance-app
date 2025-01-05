3eb02c52968b4801aafe3c9aad6166c4d9fa4084
Carlos Torres <carlosfrtr@gmail.com>
2025-01-04 22:44:58 -0300

    Add dynamic basePath and assetPrefix to Next.js configuration based on environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     env: {
         PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8080',
@@ -6,6 +8,8 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    basePath: isProd ? '/finance-app' : '',
+    assetPrefix: isProd ? process.env.NEXT_PUBLIC_SERVER_URL : '',
 };
 
 export default nextConfig;
