8faac56fedaa030eec0b256be1f706578a7646a1
Carlos Torres <carlosfrtr@gmail.com>
2025-01-06 00:09:40 -0300

    Update environment configuration and refactor Next.js settings for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
-// const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     env: {
-        PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8080',
+        PUBLIC_SERVER_URL: isProd ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:8080',
     },
+    output: 'export',
     images: {
         unoptimized: true,
     },
-    basePath: '/finance',
-    assetPrefix: "https://finance.torres.fortal.br",
+    // basePath: isProd ? '/finance' : '',
+    // assetPrefix: isProd ? "https://finance.torres.fortal.br" : "",
 };
 
 export default nextConfig;
